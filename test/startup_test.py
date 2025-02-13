370c65e9d79a20015e0f22806aa717bf83b25f2d
Sijie <sijiex@lbl.gov>
2025-02-13 10:03:57 -0800

    remove check_db_schema related functions

--- test/startup_test.py
+++ test/startup_test.py
@@ -20,29 +20,6 @@ class StartupTest(unittest.TestCase):
         catalog = Catalog(self.cUtil.getCatalogConfig())
         self.assertTrue(semantic_version.validate(catalog.version(self.cUtil.anonymous_ctx())[0]))
 
-        # Test empty startup with several different valid versions should work
-        self.cUtil.setUpEmpty(db_version=3)
-        catalog = Catalog(self.cUtil.getCatalogConfig())
-        self.assertTrue(semantic_version.validate(catalog.version(self.cUtil.anonymous_ctx())[0]))
-
-        self.cUtil.setUpEmpty(db_version=4)
-        catalog = Catalog(self.cUtil.getCatalogConfig())
-        self.assertTrue(semantic_version.validate(catalog.version(self.cUtil.anonymous_ctx())[0]))
-
-        # Startup with version that is too high should fail
-        self.cUtil.setUpEmpty(db_version=2525)
-
-        catalog = None
-        ctx = self.cUtil.user_ctx()
-        user = ctx['user_id']
-        with self.assertRaises(IOError) as e:
-            catalog = Catalog(self.cUtil.getCatalogConfig())
-            # Trigger the lazy load collection, which will call the check_db_schema() method.
-            catalog.list_favorites(ctx, user)[0]
-        self.assertEqual(str(e.exception),
-                         'Incompatible DB versions.  Expecting DB V4, found DV V2525. You are '
-                         'probably running an old version of the service.  Start up failed.')
-
     @classmethod
     def setUpClass(cls):
         print('++++++++++++ RUNNING startup_test.py +++++++++++')
